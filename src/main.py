5d68741e55f48cfe369f8d4cfba1b62c71253160
Audel Diaz <audel.diaz@outlook.com>
2021-01-26 18:45:22 -0500

    Move files to src dir

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, Request
 from sqlalchemy.orm import Session
 
 import crud
@@ -20,6 +20,15 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+@app.get("/")
+def read_root(request: Request):
+    return {
+        "Developed by": "https://github.com/AudelDiaz/",
+        "Docs": f"{request.url}docs",
+        "Redoc": f"{request.url}redoc",
+    }
 
 
 @app.get("/api/v1/friends/", response_model=List[schemas.Friend])
@@ -49,7 +58,8 @@ def get_birthdays_by_month(month: int, db: Session = Depends(get_db)):
     if 1 <= month <= 12:
         return crud.get_birthdays(db=db, month=month)
     else:
-        raise HTTPException(status_code=400, detail="The month param should be a number between 1 and 12.")
+        raise HTTPException(
+            status_code=400, detail="The month param should be a number between 1 and 12.")
 
 
 @app.get("/api/v1/friends/birthdays/month/{month}/day/{day}", response_model=List[schemas.Friend])
@@ -57,9 +67,11 @@ def get_birthday_by_day(month: int, day: int, db: Session = Depends(get_db)):
     if 1 <= month <= 12 and 1 <= day <= 31:
         return crud.get_birthdays(db=db, month=month, day=day)
     elif 1 <= month <= 12:
-        raise HTTPException(status_code=400, detail="The day param should be a number between 1 and 31.")
+        raise HTTPException(
+            status_code=400, detail="The day param should be a number between 1 and 31.")
     else:
-        raise HTTPException(status_code=400, detail="The month param should be a number between 1 and 12.")
+        raise HTTPException(
+            status_code=400, detail="The month param should be a number between 1 and 12.")
 
 
 @app.get("/api/v1/friends/birthdays/today/", response_model=List[schemas.Friend])
